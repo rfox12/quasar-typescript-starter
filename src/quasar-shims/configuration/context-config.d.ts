e1e65d39076b0a7c9adf39b5dc9bc7eb8f278360
Paolo Caleffi <p.caleffi@dreamonkey.com>
2019-11-19 12:40:50 +0100

    feat(types): enhance context object typings

--- src/quasar-shims/configuration/context-config.d.ts
+++ src/quasar-shims/configuration/context-config.d.ts
@@ -10,63 +10,117 @@ declare module 'quasar' {
     | 'capacitor'
     | 'electron';
 
-  interface QuasarContext {
+  interface BaseQuasarContext {
     /** True if we are in development mode */
     dev: boolean;
     /** True if we are in production mode */
     prod: boolean;
     /** App mode */
-    mode: { [index in QuasarModes]: boolean };
+    // TODO: find a way to model "exactly one property between the given ones"
+    //  Similar to "AtLeastOne", but must allow only one property
+    mode: { [index in QuasarModes]?: true };
     modeName: QuasarModes;
+    /** True if debugging is enabled */
+    debug: boolean;
+  }
+
+  interface CapacitorQuasarContext extends BaseQuasarContext {
+    // TODO: is there a way to automatize this two properties?
+    mode: { capacitor: true };
+    modeName: 'capacitor';
     /**
-     * App target, required only for Cordova, Capacitor and Electron modes.
+     * App target.
      *
-     * @default 'all installed' For Cordova mode
-     * @default 'none' For Capacitor
-     * @default 'current system' For Electron mode
+     * @default 'none'
      */
-    // TODO: must be present only if mode is cordova, capacitor or electron
-    // TODO: must have different possible targets depending on which mode it has and its bundler
+    // TODO: find a way to model "exactly one property between the given ones"
     target: {
-      [index in
-        | QuasarCordovaTargets
-        | QuasarCapacitorTargets
-        | QuasarBuilderTargets
-        | QuasarPackagerTargets]: boolean;
+      [index in QuasarCapacitorTargets]?: true;
     };
+    /** App target name. */
+    targetName: QuasarCapacitorTargets;
+  }
+
+  interface CordovaQuasarContext extends BaseQuasarContext {
+    // TODO: is there a way to automatize this two properties?
+    mode: { cordova: true };
+    modeName: 'cordova';
     /**
-     * App target name, required only for Cordova and Capacitor modes.
+     * App target.
+     *
+     * @default 'all installed'
      */
-    // TODO: must be present only if mode is cordova, capacitor or electron
-    // TODO: must have different possible targets depending on which mode it has and its bundler
-    targetName?:
-      | QuasarCordovaTargets
-      | QuasarCapacitorTargets
-      | QuasarBuilderTargets
-      | QuasarPackagerTargets;
+    // TODO: find a way to model "exactly one property between the given ones"
+    target: {
+      [index in QuasarCordovaTargets]?: true;
+    };
+    /** App target name. */
+    targetName: QuasarCordovaTargets;
     /**
      * Emulator name, may be present only for Cordova mode.
      *
      * @example
      * 'iPhone-7', 'iPhone-X', 'iPhone-X', 'com.apple.CoreSimulator.SimRuntime.iOS-12-2'
      */
-    // TODO: must be present only if mode is cordova
     emulator: string;
-    // TODO: may be present only if mode is electron
-    // TODO: must have different possible arch depending on used bundler
-    arch: {
-      [index in QuasarBuilderArchs | QuasarPackagerArchs]: boolean;
+  }
+
+  interface ElectronQuasarContext extends BaseQuasarContext {
+    // TODO: is there a way to automatize this two properties?
+    mode: { electron: true };
+    modeName: 'electron';
+    /**
+     * App target.
+     *
+     * @default 'current system'
+     */
+    // TODO: must have different possible targets depending on which mode it has and its bundler
+    // TODO: find a way to model "exactly one property between the given ones"
+    target: {
+      [index in QuasarBuilderTargets | QuasarPackagerTargets]?: true;
     };
-    // TODO: may be present only if mode is electron
+    /** App target name. */
+    targetName: QuasarBuilderTargets | QuasarPackagerTargets;
     // TODO: must have different possible arch depending on used bundler
-    archName?: QuasarBuilderArchs | QuasarPackagerArchs;
-    // TODO: may be present only if mode is electron
-    bundler: { [index in QuasarElectronBundlers]: boolean };
-    // TODO: may be present only if mode is electron
+    // TODO: find a way to model "exactly one property between the given ones"
+    arch: {
+      [index in QuasarBuilderArchs | QuasarPackagerArchs]?: true;
+    };
+    // TODO: must have different possible arch depending on used bundler
+    archName: QuasarBuilderArchs | QuasarPackagerArchs;
+    // TODO: find a way to model "exactly one property between the given ones"
+    bundler: { [index in QuasarElectronBundlers]?: true };
     bundlerName: QuasarElectronBundlers;
-    /** True if debugging is enabled */
-    debug: boolean;
-    // TODO: may be present only if mode is electron and bundler is builder
+    // TODO: may be present only if bundler is builder
     publish?: 'onTag' | 'onTagOrDraft' | 'always' | 'never';
   }
+
+  interface SpaQuasarContext extends BaseQuasarContext {
+    // TODO: is there a way to automatize this two properties?
+    mode: { spa: true };
+    modeName: 'spa';
+  }
+
+  interface PwaQuasarContext extends BaseQuasarContext {
+    // TODO: is there a way to automatize this two properties?
+    mode: { pwa: true };
+    modeName: 'pwa';
+  }
+
+  interface SsrQuasarContext extends BaseQuasarContext {
+    // TODO: is there a way to automatize this two properties?
+    mode: { ssr: true };
+    modeName: 'ssr';
+  }
+
+  // TODO: this works nicely, but checking current mode in TS is easier with
+  //  `context.modeName === 'spa'` instead of `context.mode.spa` as it was with JS
+  // Check if there is some way to make this better
+  type QuasarContext =
+    | SpaQuasarContext
+    | HasPwa<PwaQuasarContext, never>
+    | HasSsr<SsrQuasarContext, never>
+    | HasCapacitor<CapacitorQuasarContext, never>
+    | HasCordova<CordovaQuasarContext, never>
+    | HasElectron<ElectronQuasarContext, never>;
 }
