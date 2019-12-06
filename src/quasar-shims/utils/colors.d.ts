dc580ab0c0470deb53d7339887a529dced1a3bfa
Paolo Caleffi <p.caleffi@dreamonkey.com>
2019-12-06 14:12:56 +0100

    refactor(types): remove types merged into core

--- src/quasar-shims/utils/colors.d.ts
+++
@@ -1,7 +0,0 @@
-import 'quasar';
-
-// WAITING-FOR-MERGE
-declare module 'quasar' {
-  // `null` was missing into return type.
-  function getBrand(color: string, element?: Element): string | null;
-}
--- src/quasar-shims/utils/scroll.d.ts
+++ src/quasar-shims/utils/scroll.d.ts
@@ -1,43 +1,6 @@
 import 'quasar';
 
-// WAITING-FOR-MERGE
-// As suggested by Kerry, all `Element` as been updated to `HTMLElement`
-// See https://discordapp.com/channels/616161554433572894/616164014103461899/636262196275445770
+// https://github.com/quasarframework/quasar/issues/5758#issuecomment-560454496
 declare module 'quasar' {
-  // Apparently parameter and return types where switched:
-  //  `Window` doesn't have a `.closest` method, used by the implementation.
-  function getScrollTarget(el: HTMLElement): HTMLElement | Window;
-
-  function getScrollHeight(el: HTMLElement | Window): number;
-  function getScrollWidth(el: HTMLElement | Window): number;
-
-  function getScrollPosition(scrollTarget: HTMLElement | Window): number;
-  function getHorizontalScrollPosition(
-    scrollTarget: HTMLElement | Window
-  ): number;
-
-  function animScrollTo(
-    el: HTMLElement | Window,
-    to: number,
-    duration: number
-  ): void;
-  function animHorizontalScrollTo(
-    el: HTMLElement | Window,
-    to: number,
-    duration: number
-  ): void;
-
-  function setScrollPosition(
-    scrollTarget: HTMLElement | Window,
-    offset: number,
-    duration?: number
-  ): void;
-  function setHorizontalScrollPosition(
-    scrollTarget: HTMLElement | Window,
-    offset: number,
-    duration?: number
-  ): void;
-
-  function getScrollbarWidth(): number;
-  function hasScrollbar(el: HTMLElement | Window, onY?: boolean): boolean;
+  function getScrollTarget(el: Element): HTMLElement | Window;
 }
