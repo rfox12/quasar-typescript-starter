3d6693e1f73e8d85cd1c0e43c4ab31a001237fb1
Paolo Caleffi <p.caleffi@dreamonkey.com>
2019-12-13 11:16:03 +0100

    refactor(types): remove types merged into core

--- src/quasar-shims/globals.d.ts
+++ src/quasar-shims/globals.d.ts
@@ -1,30 +1,17 @@
-import 'quasar';
+import {
+  QVueGlobals as BaseQVueGlobals,
+  HasCapacitor,
+  HasCordova,
+  HasElectron
+} from 'quasar';
 import * as Cordova from 'cordova';
 import * as electron from 'electron';
 import { Capacitor } from '../../src-capacitor/node_modules/@capacitor/core';
 
-declare module 'quasar' {
-  // WAITING-FOR_MERGE - https://github.com/quasarframework/quasar/pull/5735
-  interface GlobalQuasarLanguage extends QuasarLanguage {
-    set(lang: QuasarLanguage): void;
-    /** Returns undefined when in SSR mode or when it cannot determine current language. */
-    getLocale(): string | undefined;
-  }
-
-  // WAITING-FOR_MERGE - https://github.com/quasarframework/quasar/pull/5735
-  interface GlobalQuasarIconSet extends QuasarIconSet {
-    set(iconSet: QuasarIconSet): void;
-  }
-
-  // HasCapacitor, HasCordova and HasElectron are hidden behind feature-flags
-  // Capacitor typings are into @capacitor/core,
-  //  will it be possible to retain types also inside it?
-  interface QVueGlobals
-    extends HasCapacitor<{ capacitor: typeof Capacitor }>,
-      HasCordova<{ cordova: typeof Cordova }>,
-      HasElectron<{ electron: typeof electron }> {
-    version: string;
-    lang: GlobalQuasarLanguage;
-    iconSet: GlobalQuasarIconSet;
-  }
-}
+// HasCapacitor, HasCordova and HasElectron are hidden behind feature-flags
+// Capacitor typings are into @capacitor/core,
+//  will it be possible to retain types also inside it?
+export type QVueGlobals = BaseQVueGlobals &
+  HasCapacitor<{ capacitor: typeof Capacitor }> &
+  HasCordova<{ cordova: typeof Cordova }> &
+  HasElectron<{ electron: typeof electron }>;
--- src/quasar-shims/utils.ts
+++
@@ -1,4 +0,0 @@
-// WAITING-FOR_MERGE - https://github.com/quasarframework/quasar/pull/5735
-export type StringDictionary<T extends string> = Required<
-  { [index in T]: string }
->;
